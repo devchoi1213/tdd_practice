1718471bec0fec07b13f5d7dbf01e7f9bf7c0293
devchoi1213 <devchoi1213@gmail.com>
2023-04-02 14:46:25 +0900

    mock을 이용한 테스트 코드 작성

---
+++ ProductImporter/src/test/ProductSynchronizerUsingMock.test.ts
@@ -0,0 +1,36 @@
+import {describe, test} from '@jest/globals';
+import WayneEnterprisesProductImporter from "../suppliers/wayneenterprises/WayneEnterprisesProductImporter";
+import ProductSynchronizer from "../ProductSynchronizer";
+import ListPriceFilter from "../ListPriceFilter";
+import ProductInventorySpy from "./spy/ProductInventorySpy";
+import Pricing from "../Pricing";
+import Product from "../Product";
+import {instance, mock, verify, when} from "ts-mockito";
+import ProductValidator from "../ProductValidator";
+
+describe('ProductSynchronizer test using mock', () => {
+  test('sut does not save invalid product', () => {
+    // Arrange
+    const pricing = new Pricing(10, 1);
+    const product = new Product('supplierName', 'productCode', 'productName', pricing);
+
+    const importerMock: WayneEnterprisesProductImporter = mock(WayneEnterprisesProductImporter);
+    when(importerMock.fetchProduct()).thenReturn([product]);
+    const importer = instance(importerMock);
+
+    const validatorMock: ProductValidator = mock(ListPriceFilter);
+    when(validatorMock.isValid(product)).thenReturn(false);
+    const validator = instance(validatorMock);
+
+    const inventoryMock: ProductInventorySpy = mock(ProductInventorySpy);
+    const inventory = instance(inventoryMock)
+
+    const sut = new ProductSynchronizer(importer, validator, inventory);
+
+    // Act
+    sut.run();
+
+    // Assert
+    verify(inventoryMock.upsertProduct(product)).never();
+  });
+});
